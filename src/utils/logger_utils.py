248b646c7298049f5a3fd55e8f9eb4dc814b8d3d
VictorVangeli <whgaleon1@yandex.ru>
2025-02-11 11:22:26 +0300

    fix: change logic to create logger

--- src/utils/logger_utils.py
+++ src/utils/logger_utils.py
@@ -1,56 +1,68 @@
-import logging
-import os
-
-from src.core.const import LOG_FILE_PATH
-
-
-def clear_log_file(log_file_path: str):
-    """
-    Очищает содержимое файла логов.
-
-    :param log_file_path: Путь к файлу логов, который нужно очистить.
-    :type log_file_path: str
-    """
-    temp_logger = logging.getLogger("temp_logger")
-    temp_logger.setLevel(logging.INFO)
-    temp_logger.addHandler(logging.StreamHandler())
-
-    if os.path.exists(log_file_path):
-        with open(log_file_path, "w") as log_file:
-            log_file.write("")
-        temp_logger.debug(f"Файл {log_file_path} успешно очищен.")
-
-
-def create_log_handlers(log_file_path: str):
-    """
-    Создаёт обработчики для логгера.
-    :param log_file_path: Путь к файлу логов.
-    :return: Список обработчиков.
-    """
-    file_handler = logging.FileHandler(log_file_path)
-    stream_handler = logging.StreamHandler()
-    return [file_handler, stream_handler]
-
-
-def setup_logger():
-    """
-    Настраивает логгер для приложения.
-    """
-    clear_log_file(LOG_FILE_PATH)
-
-    logging.basicConfig(
-        # Необходимо скорректировать логику таким образом, чтобы динамически ссылаться на уровень логирования из
-        # раннера или Dockerfile (что мне кажется логичнее)
-        level=logging.INFO,
-        format="%(asctime)s [%(levelname)s] %(message)s",
-        # Расположение `LOG_FILE_PATH` сейчас находится в CORE, но предполагаю, что ты захочешь переместить в другое
-        # место
-        handlers=create_log_handlers(LOG_FILE_PATH),
-    )
-
-    return logging.getLogger(__name__)
-
-
-# Не понимаю как в твоей структуре правильно определить экземпляр логгера. Тут или где-то в `core`, поэтому пока оставлю
-# тут
-logger = setup_logger()
+# import logging
+# from logging.handlers import RotatingFileHandler
+# from pathlib import Path
+# from dynaconf import Dynaconf
+#
+#
+# class LoggerUtils:
+#     def __init__(self):
+#         self.log_dir = Path(settings.logging.dir)
+#         self.log_file = self.log_dir / settings.logging.file
+#         self.log_level = getattr(logging, settings.logging.level, logging.INFO)
+#         self.max_log_size = settings.logging.max_size
+#         self.backup_count = settings.logging.backup_count
+#
+#         self._setup_logging_directory()
+#
+#     def setup_logging_directory(self):
+#         """Создаёт папку для логов, если её нет."""
+#         self.log_dir.mkdir(parents=True, exist_ok=True)
+#
+#
+#     def get_console_handler(self) -> logging.Handler:
+#         """Создаёт обработчик логов для консоли."""
+#         console_handler = logging.StreamHandler()
+#         console_handler.setLevel(LOG_LEVEL)
+#         console_handler.setFormatter(get_log_formatter())
+#         return console_handler
+#
+#
+#     def get_file_handler() -> logging.Handler:
+#         """Создаёт обработчик логов для файла с ротацией."""
+#         file_handler = RotatingFileHandler(
+#             filename=LOG_FILE,
+#             encoding="utf-8",
+#             maxBytes=MAX_SIZE,
+#             backupCount=BACKUP_COUNT
+#         )
+#         file_handler.setLevel(LOG_LEVEL)
+#         file_handler.setFormatter(get_log_formatter())
+#         return file_handler
+#
+#
+#     def get_log_formatter() -> logging.Formatter:
+#         """Возвращает форматтер для логов."""
+#         return logging.Formatter(
+#             fmt="%(asctime)s - [%(levelname)s] - %(name)s - "
+#                 "(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
+#         )
+#
+#
+#     def get_logger(name: str | None = None) -> logging.Logger:
+#         """
+#         Возвращает настроенный логгер.
+#
+#         :param name: __name__ файла, вызывающего логгер
+#         :return: Объект логгера
+#         """
+#         setup_logging_directory()
+#
+#         logger = logging.getLogger(name)
+#
+#         if not logger.hasHandlers():
+#             logger.setLevel(LOG_LEVEL)
+#             logger.addHandler(get_console_handler())
+#             logger.addHandler(get_file_handler())
+#
+#         return
+#
