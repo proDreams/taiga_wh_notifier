f40d5b8d2b83c1b41f52678b797215c7d1a28ddd
VictorVangeli <whgaleon1@yandex.ru>
2025-02-11 13:06:34 +0300

    fix: rework logic from `LoggerUtils`

--- src/utils/logger_utils.py
+++ src/utils/logger_utils.py
@@ -1,68 +1,102 @@
-# import logging
-# from logging.handlers import RotatingFileHandler
-# from pathlib import Path
-# from dynaconf import Dynaconf
-#
-#
-# class LoggerUtils:
-#     def __init__(self):
-#         self.log_dir = Path(settings.logging.dir)
-#         self.log_file = self.log_dir / settings.logging.file
-#         self.log_level = getattr(logging, settings.logging.level, logging.INFO)
-#         self.max_log_size = settings.logging.max_size
-#         self.backup_count = settings.logging.backup_count
-#
-#         self._setup_logging_directory()
-#
-#     def setup_logging_directory(self):
-#         """Создаёт папку для логов, если её нет."""
-#         self.log_dir.mkdir(parents=True, exist_ok=True)
-#
-#
-#     def get_console_handler(self) -> logging.Handler:
-#         """Создаёт обработчик логов для консоли."""
-#         console_handler = logging.StreamHandler()
-#         console_handler.setLevel(LOG_LEVEL)
-#         console_handler.setFormatter(get_log_formatter())
-#         return console_handler
-#
-#
-#     def get_file_handler() -> logging.Handler:
-#         """Создаёт обработчик логов для файла с ротацией."""
-#         file_handler = RotatingFileHandler(
-#             filename=LOG_FILE,
-#             encoding="utf-8",
-#             maxBytes=MAX_SIZE,
-#             backupCount=BACKUP_COUNT
-#         )
-#         file_handler.setLevel(LOG_LEVEL)
-#         file_handler.setFormatter(get_log_formatter())
-#         return file_handler
-#
-#
-#     def get_log_formatter() -> logging.Formatter:
-#         """Возвращает форматтер для логов."""
-#         return logging.Formatter(
-#             fmt="%(asctime)s - [%(levelname)s] - %(name)s - "
-#                 "(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
-#         )
-#
-#
-#     def get_logger(name: str | None = None) -> logging.Logger:
-#         """
-#         Возвращает настроенный логгер.
-#
-#         :param name: __name__ файла, вызывающего логгер
-#         :return: Объект логгера
-#         """
-#         setup_logging_directory()
-#
-#         logger = logging.getLogger(name)
-#
-#         if not logger.hasHandlers():
-#             logger.setLevel(LOG_LEVEL)
-#             logger.addHandler(get_console_handler())
-#             logger.addHandler(get_file_handler())
-#
-#         return
-#
+import logging
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+
+import dynaconf
+
+from src.core.Base.singleton import Singleton
+
+
+class LoggerUtils(Singleton):
+    def __init__(self, settings: dynaconf.Dynaconf):
+        self.log_dir = Path(settings.LOG_DIR)
+        self.log_file = self.log_dir / settings.LOG_FILE
+        self.log_level = settings.LOG_LEVEL
+        self.max_log_size = settings.MAX_SIZE_MB
+        self.backup_count = settings.BACKUP_COUNT
+
+        self._setup_logging_directory()
+
+    def _setup_logging_directory(self):
+        """
+        Sets up the logging directory.
+
+        This method ensures that the directory specified by `self.log_dir` exists.
+        If the directory already exists or cannot be created due to permissions issues,
+        it handles these cases gracefully without raising an exception.
+
+        :returns: True if the directory was successfully created or already existed; False otherwise.
+        :rtype: bool
+        """
+        self.log_dir.mkdir(parents=True, exist_ok=True)
+
+    def _get_console_handler(self) -> logging.Handler:
+        """
+        Returns a logging handler for the console.
+
+        :returns: A new logging handler configured to log to the console.
+        :rtype: logging.Handler
+        """
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(self.log_level)
+        console_handler.setFormatter(self.get_log_formatter())
+        return console_handler
+
+    def _get_file_handler(self) -> logging.Handler:
+        """
+        Retrieves a rotating file handler for logging.
+
+        :returns: A logging handler that writes logs to a rotating file.
+        :rtype: logging.Handler
+        """
+        file_handler = RotatingFileHandler(
+            filename=self.log_file,
+            encoding="utf-8",
+            maxBytes=self.max_log_size * 1024 * 1024,
+            backupCount=self.backup_count,
+        )
+        file_handler.setLevel(self.log_level)
+        file_handler.setFormatter(self.get_log_formatter())
+        return file_handler
+
+    @staticmethod
+    def get_log_formatter() -> logging.Formatter:
+        """
+        Returns a logging formatter with a specific format for log messages.
+
+        This formatter includes the following information in each log message:
+        - The timestamp (asctime)
+        - The logging level (levelname)
+        - The name of the logger (name)
+        - The filename where the log record was created (filename)
+        - The function name where the log record was created (funcName)
+        - The line number where the log record was created (lineno)
+        - The actual log message (message)
+
+        :returns: A logging formatter instance with the specified format.
+        :rtype: logging.Formatter
+        """
+        return logging.Formatter(
+            fmt="%(asctime)s - [%(levelname)s] - %(name)s - " "(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
+        )
+
+    def get_logger(self, name: str | None = None) -> logging.Logger:
+        """
+        Returns a logger instance with the specified name.
+
+        :param name: The name of the logger. If None, the root logger is returned.
+        :type name: str | None
+
+        :returns: A logger instance configured with handlers and logging level.
+        :rtype: logging.Logger
+        :raises ValueError: If the specified logging directory could not be set up.
+        """
+
+        logger = logging.getLogger(name)
+
+        if not logger.hasHandlers():
+            logger.setLevel(self.log_level)
+            logger.addHandler(self._get_console_handler())
+            logger.addHandler(self._get_file_handler())
+
+        return logger
