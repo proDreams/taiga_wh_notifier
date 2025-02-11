3ae781c59ba6ebde8f2b95f5fcacbaf374643832
VictorVangeli <whgaleon1@yandex.ru>
2025-02-11 14:07:06 +0300

    fix: update logic to `get_strings`

--- src/utils/yaml_utils.py
+++ src/utils/yaml_utils.py
@@ -3,14 +3,14 @@ from pathlib import Path
 import yaml
 
 
-def get_strings() -> dict[str, str | dict[str, str | list]]:
+def get_strings(path: str) -> dict[str, str | dict[str, str | list]]:
     """
     Функция чтения всех YAML-файлов в директории strings и возврате единого словаря.
 
     :return: Словарь с содержимым YAML-файлов.
     """
     strings_dict = {}
-    for path in Path("strings").glob("*.yaml"):
+    for path in Path(path).glob("*.yaml"):
         with open(path, encoding="utf-8") as f:
             strings_dict.update({path.stem: dict(yaml.safe_load(f))})
 
