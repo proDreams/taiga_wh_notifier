49dc1e50432e4fc3e0a3c1956a6d9458dba61722
proDream <sushkoos@gmail.com>
2025-02-13 04:01:15 +0400

    feat: added Redis dependency for message broker
    - Created a new file `redis_dependency.py` in `src/infrastructure/broker`.
    - Implemented `RedisSessionDependency` class that inherits from `Singleton`.
    - Added methods to initialize the connection pool and manage sessions asynchronously.
    - Defined an asynchronous context manager for handling Redis sessions.

---
+++ src/infrastructure/broker/redis_dependency.py
@@ -0,0 +1,24 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+
+from redis.asyncio import ConnectionPool, Redis
+
+from src.core.Base.singleton import Singleton
+from src.core.settings import Configuration
+
+
+class RedisSessionDependency(Singleton):
+    def __init__(self) -> None:
+        self._url = Configuration.settings.REDIS_URL
+        self._pool: ConnectionPool = self._init_pool()
+
+    def _init_pool(self) -> ConnectionPool:
+        return ConnectionPool.from_url(self._url, encoding="utf-8", decode_responses=True, max_connections=20)
+
+    @asynccontextmanager
+    async def session(self) -> AsyncGenerator:
+        redis_client = Redis(connection_pool=self._pool)
+        try:
+            yield redis_client
+        finally:
+            await redis_client.close()
