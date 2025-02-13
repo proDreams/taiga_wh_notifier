0a112cf38a636d37664d9130e2a0a9d5df59f450
VictorVangeli <whgaleon1@yandex.ru>
2025-02-13 17:23:59 +0300

    feat: add pydantic schemas to webhook payload

---
+++ src/entities/schemas/webhook_data/base_schemas.py
@@ -0,0 +1,68 @@
+from datetime import date, datetime
+
+from pydantic import BaseModel, Field
+
+
+class BaseID(BaseModel):
+    id: int
+
+
+class BasePermalink(BaseModel):
+    permalink: str
+
+
+class BaseName(BaseModel):
+    name: str
+
+
+class TimeStamped(BaseModel):
+    created_date: datetime | None
+    modified_date: datetime | None
+
+
+class DiffMilestone(BaseModel):
+    from_: str | None = Field(default=None, alias="from")
+    to: str | None
+
+
+class DiffSprintOrder(BaseModel):
+    from_: int | None = Field(default=None, alias="from")
+    to: int | None
+
+
+class DiffDueDate(BaseModel):
+    from_: date | None = Field(default=None, alias="from")
+    to: date | None
+
+
+class DiffStatus(BaseModel):
+    from_: str | None = Field(default=None, alias="from")
+    to: str | None
+
+
+class DiffAttachment(BaseModel):
+    id: int | None = None
+    filename: str | None = None
+    url: str | None = None
+    is_deprecated: bool | None = None
+    description: str | list[str, str] | None = None
+
+
+class DiffAttachments(BaseModel):
+    new: list[DiffAttachment] | None = None
+    changed: list[DiffAttachment] | None = None
+    deleted: list[DiffAttachment] | None = None
+
+
+class Diff(BaseModel):
+    milestone: DiffMilestone | None = None
+    sprint_order: DiffSprintOrder | None = None
+    due_date: DiffDueDate | None = None
+    status: DiffStatus | None = None
+    attachments: DiffAttachments | None = None
+
+
+class Change(BaseModel):
+    comment: str | None
+    delete_comment_date: datetime | None
+    diff: Diff | None
---
+++ src/entities/schemas/webhook_data/nested_schemas.py
@@ -0,0 +1,183 @@
+from datetime import date, datetime
+from typing import Any
+
+from src.entities.schemas.webhook_data.base_schemas import (
+    BaseID,
+    BaseName,
+    BasePermalink,
+    TimeStamped,
+)
+
+
+class BaseEntity(BaseID, BasePermalink):
+    """
+    Nested:
+     - id from BaseID;
+     - permalink from BasePermalink;
+    """
+
+    pass
+
+
+class User(BaseEntity):
+    """
+    Nested:
+     - id from BaseEntity;
+     - permalink from BaseEntity;
+    """
+
+    username: str
+    full_name: str
+    photo: str
+    gravatar_id: str
+
+
+class Project(BaseEntity):
+    """
+    Nested:
+     - id from BaseEntity;
+     - permalink from BaseEntity;
+    """
+
+    name: str
+    logo_big_url: str | None = None
+
+
+class Status(BaseID, BaseName):
+    """
+    Nested:
+     - id from BaseID;
+     - name from BaseName;
+    """
+
+    slug: str
+    color: str
+    is_closed: bool
+    is_archived: bool | None = None
+
+
+class Milestone(BaseEntity, TimeStamped, BaseName):
+    """
+    Nested:
+     - id from BaseEntity;
+     - permalink from BaseEntity;
+     - created_date from TimeStamped;
+     - name from BaseName;
+    """
+
+    slug: str
+    estimated_start: date  # Формат "YYYY-MM-DD"
+    estimated_finish: date
+    closed: bool
+    disponibility: float
+    project: Project
+    owner: User
+
+
+class BaseItem(BaseEntity, TimeStamped):
+    """
+    Nested:
+     - id from BaseEntity;
+     - permalink from BaseEntity;
+     - created_date from TimeStamped;
+     - modified_date from TimeStamped;
+     - name from BaseName;
+    """
+
+    custom_attributes_values: dict[str, Any]
+    ref: int
+    due_date: datetime | None
+    due_date_reason: str
+    subject: str
+    watchers: list[Any] = []
+    is_blocked: bool
+    blocked_note: str
+    description: str
+    tags: list[Any] = []
+    project: Project
+    owner: User
+    assigned_to: User | None
+    status: Status
+    milestone: Milestone
+
+
+class Point(BaseName):
+    """
+    Nested:
+     - name from BaseName;
+    """
+
+    role: str
+    value: float | None
+
+
+class UserStory(BaseItem):
+    """
+    Nested from BaseItem:
+        - id
+        - permalink
+        - created_date
+        - modified_date
+        - name
+        - custom_attributes_values
+        - ref
+        - due_date
+        - due_date_reason
+        - subject
+        - watchers
+        - is_blocked
+        - blocked_note
+        - description
+        - tags
+        - project
+        - owner
+        - assigned_to
+        - status
+        - milestone
+    """
+
+    is_closed: bool
+    finish_date: datetime | None
+    client_requirement: bool
+    team_requirement: bool
+    generated_from_issue: Any | None
+    generated_from_task: Any | None
+    from_task_ref: Any | None
+    external_reference: Any | None
+    tribe_gig: Any | None
+    assigned_users: list[int]
+    points: list[Point] = []
+
+
+class Task(BaseItem):
+    """
+    Nested from BaseItem:
+        - id
+        - permalink
+        - created_date
+        - modified_date
+        - name
+        - custom_attributes_values
+        - ref
+        - due_date
+        - due_date_reason
+        - subject
+        - watchers
+        - is_blocked
+        - blocked_note
+        - description
+        - tags
+        - project
+        - owner
+        - assigned_to
+        - status
+        - milestone
+    """
+
+    finished_date: datetime | None
+    us_order: int
+    taskboard_order: int
+    is_iocaine: bool
+    external_reference: Any | None
+    user_story: UserStory
+    promoted_to: list[Any] = []
