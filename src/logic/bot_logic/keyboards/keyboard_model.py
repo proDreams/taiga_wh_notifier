11676943eae36f6e7ddd59374e154594df9c06f0
VictorVangeli <whgaleon1@yandex.ru>
2025-02-13 21:20:42 +0300

    feat: added support multilanguage in `KeyboardsGenerator`

--- src/logic/bot_logic/keyboards/keyboard_model.py
+++ src/logic/bot_logic/keyboards/keyboard_model.py
@@ -5,32 +5,37 @@ from aiogram.types import (
     ReplyKeyboardMarkup,
 )
 
+from src.core.settings import Configuration
+
+# keyboards = KeyboardGenerator(
+#     buttons_data=strings.get("keyboards_buttons").get("buttons"),
+#     language_data=Configuration.strings.get("keyboards_language").get("language"),
+# )
+
 
 class KeyboardGenerator:
-    def __init__(self, buttons_data: dict, keyboard_type: str = "inline"):
+    def __init__(self):
         """
         Инициализация генератора клавиатур с загрузкой данных из YAML.
-        :param buttons_data: Словарь с доступными кнопками из конфигурационного файла клавиатуры.
-        :param keyboard_type: Тип клавиатуры ("inline" или "reply").
         """
-        self.buttons = buttons_data
-        self.keyboard_type = keyboard_type.lower()
+        self.buttons = Configuration.strings.get("keyboards_buttons").get("buttons")
+        self.language_data = Configuration.strings.get("keyboards_language").get("language")
 
-    def create_keyboard(self, keys: list, keyboard_type: str = "inline", row_width: int = 1):
+    def create_static_keyboard(self, key: str, lang: str):
         """
         Создаёт клавиатуру на основе переданных ключей и типа клавиатуры.
-        :param keys: Список ключей кнопок из YAML.
-        :param keyboard_type: Тип клавиатуры ("inline" или "reply").
-        :param row_width: Количество кнопок в ряду.
+        :param key: Список ключей кнопок из YAML.
+        :param lang: Язык системы пользователя.
         :return: Объект InlineKeyboardMarkup или ReplyKeyboardMarkup.
         """
-        self._validate_keyboard_type(keyboard_type)
-        buttons = self.create_buttons(keys, keyboard_type)
-        rows = self._group_buttons(buttons, row_width)
+        data = Configuration.strings.get("keyboards_list").get(key)
+        self._validate_keyboard_type(data.get("keyboard_type"))
+        buttons = self.create_buttons(data.get("key"), data.get("keyboard_type"), lang=lang)
+        rows = self._group_buttons(buttons, data.get("row_width"))
 
-        if keyboard_type == "inline":
+        if data.get("keyboard_type") == "inline":
             return InlineKeyboardMarkup(inline_keyboard=rows)
-        elif keyboard_type == "reply":
+        elif data.get("keyboard_type") == "reply":
             return ReplyKeyboardMarkup(keyboard=rows, resize_keyboard=True)
 
     @staticmethod
@@ -42,7 +47,7 @@ class KeyboardGenerator:
         if keyboard_type not in ["inline", "reply"]:
             raise ValueError("Тип клавиатуры должен быть 'inline' или 'reply'.")
 
-    def create_buttons(self, keys: list, keyboard_type: str) -> list:
+    def create_buttons(self, keys: list, keyboard_type: str, lang: str) -> list:
         """
         Создаёт список кнопок на основе переданных ключей и типа клавиатуры.
 
@@ -51,17 +56,21 @@ class KeyboardGenerator:
             create_buttons(keys, "inline")
             -> [InlineKeyboardButton(...), ...]
 
+        :param lang: Системный язык пользователя
         :param keys: Список ключей кнопок из YAML.
         :param keyboard_type: Тип клавиатуры ("inline" или "reply").
         :return: Список кнопок.
         :raises ValueError: Если тип клавиатуры неверен или отсутствуют обязательные поля.
         """
-        button_creators = {"inline": self._create_inline_button, "reply": self._create_reply_button}
+        button_creators = {
+            "inline": self._create_inline_button,
+            "reply": self._create_reply_button,
+        }
 
         if keyboard_type not in button_creators:
             raise ValueError(f"Недопустимый тип клавиатуры: {keyboard_type}")
 
-        return [button_creators[keyboard_type](key) for key in keys]
+        return [button_creators[keyboard_type](key=key, lang=lang) for key in keys]
 
     def get_button_info(self, key: str):
         """
@@ -73,7 +82,11 @@ class KeyboardGenerator:
             raise ValueError(f"Кнопка с ключом '{key}' не найдена в YAML.")
         return self.buttons[key]
 
-    def _create_inline_button(self, key: str) -> InlineKeyboardButton:
+    def _translate_button_text(self, key: str, lang: str) -> str:
+        translated_text = self.language_data
+        return translated_text.get(lang, {}).get(key, key)
+
+    def _create_inline_button(self, key: str, lang: str) -> InlineKeyboardButton:
         """Создает Inline-кнопку с проверкой обязательных параметров."""
         button_info = self.get_button_info(key)
 
@@ -82,15 +95,17 @@ class KeyboardGenerator:
 
         if not ("url" in button_info or "callback_data" in button_info):
             raise ValueError(f"Кнопка '{key}': требуется 'url' или 'callback_data'")
+        translated_text = self._translate_button_text(button_info.get("text"), lang=lang)
 
         return InlineKeyboardButton(
-            text=button_info["text"], url=button_info.get("url"), callback_data=button_info.get("callback_data")
+            text=translated_text, url=button_info.get("url"), callback_data=button_info.get("callback_data")
         )
 
-    def _create_reply_button(self, key: str) -> KeyboardButton:
+    def _create_reply_button(self, key: str, lang: str) -> KeyboardButton:
         """Создает KeyboardButton."""
         button_info = self.get_button_info(key)
-        return KeyboardButton(text=button_info["text"])
+        translated_text = self._translate_button_text(button_info.get("text"), lang=lang)
+        return KeyboardButton(text=translated_text)
 
     @staticmethod
     def _group_buttons(buttons: list, row_width: int) -> list:
--- src/logic/bot_logic/keyboards/keyboards_stash.py
+++
@@ -1,3 +0,0 @@
-from src.core.settings import Configuration
-
-started_keyboard = Configuration.keyboards.create_keyboard(keys=["get_start"], keyboard_type="reply", row_width=1)
