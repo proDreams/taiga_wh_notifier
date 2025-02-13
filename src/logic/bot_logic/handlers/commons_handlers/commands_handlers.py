e3789ab870df1ba96aaf2fa72af04d97de1026f3
VictorVangeli <whgaleon1@yandex.ru>
2025-02-13 21:29:19 +0300

    fix: update way to create keyboard

--- src/logic/bot_logic/handlers/commons_handlers/commands_handlers.py
+++ src/logic/bot_logic/handlers/commons_handlers/commands_handlers.py
@@ -2,19 +2,21 @@ from aiogram import F, Router
 from aiogram.types import Message
 
 from src.core.settings import Configuration
-from src.logic.bot_logic.keyboards.keyboards_stash import started_keyboard
+from src.logic.bot_logic.keyboards.keyboard_model import KeyboardGenerator
 
 router = Router()
 
 
 @router.message(F.text == "/start")
-async def start_handler(message: Message) -> None:
+async def start_handler(message: Message, keyboard: KeyboardGenerator = KeyboardGenerator()) -> None:
     """
     Handles the '/start' command by sending a welcome message.
 
+    :param keyboard:
     :param message: The incoming message object containing the '/start' command.
     :type message: Message
     """
     await message.answer(
-        Configuration.strings.get("messages_text").get("message_to_start"), reply_markup=started_keyboard
+        Configuration.strings.get("messages_text").get("message_to_start"),
+        reply_markup=keyboard.create_static_keyboard(key="started_keyboard", lang="en"),
     )
