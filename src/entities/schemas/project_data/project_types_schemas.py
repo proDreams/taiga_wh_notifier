0d56be1387d7c5597466006995c6fb783d57bbff
proDream <sushkoos@gmail.com>
2025-02-14 04:55:15 +0400

    refactor: streamlined project type schema definitions
    - Removed redundant `BaseProjectTypeSchema` and related classes.
    - Updated `ProjectTypeCreateSchema` to include `project_id` with validation.
    - Combined `TestSchema`, `MilestoneSchema`, `UserStorySchema`, and `TaskSchema` into `ProjectTypeSchema`.

--- src/entities/schemas/project_data/project_types_schemas.py
+++ src/entities/schemas/project_data/project_types_schemas.py
@@ -1,30 +1,18 @@
-from pydantic import BaseModel
+from typing import Annotated
+
+from pydantic import BaseModel, BeforeValidator
 
 from src.entities.enums.event_enums import EventTypeEnum
 from src.entities.schemas.project_data.base_project_schemas import ProjectIDSchema
+from src.entities.schemas.validators.project_validators import validate_object_id
 
 
-class BaseProjectTypeSchema(BaseModel):
-    type: EventTypeEnum
+class ProjectTypeCreateSchema(BaseModel):
+    event_type: EventTypeEnum
     chat_id: int | list[int]
     thread_id: int | list[int] | None = None
+    project_id: Annotated[str, BeforeValidator(validate_object_id)]
 
 
-class TestSchema(ProjectIDSchema, BaseProjectTypeSchema):
-    pass
-
-
-class MilestoneSchema(ProjectIDSchema, BaseProjectTypeSchema):
-    pass
-
-
-class UserStorySchema(ProjectIDSchema, BaseProjectTypeSchema):
-    pass
-
-
-class TaskSchema(ProjectIDSchema, BaseProjectTypeSchema):
-    pass
-
-
-class IssueSchema(ProjectIDSchema, BaseProjectTypeSchema):
+class ProjectTypeSchema(ProjectIDSchema, ProjectTypeCreateSchema):
     pass
