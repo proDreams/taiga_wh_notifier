54e1c27c496e90c676806ec973ec6c8cd44bf4f1
proDream <sushkoos@gmail.com>
2025-02-17 23:53:53 +0400

    refactor: update route file name and register middleware in bot router
    - Renamed `update_router.py` to `update_route.py`.
    - Added function `register_bot_middlewares()` to include `UserMiddleware` in the dispatcher.
    - Updated import statements accordingly.

--- src/presentation/bot_routers/init_router.py
+++ src/presentation/bot_routers/init_router.py
@@ -1,6 +1,11 @@
 from src.core.settings import Configuration
 from src.logic.bot_logic.handlers import handlers_router
+from src.logic.bot_logic.middlewares.user_middleware import UserMiddleware
 
 
-async def register_bot_routers():
+async def register_bot_routers() -> None:
     Configuration.dispatcher.include_router(handlers_router)
+
+
+async def register_bot_middlewares() -> None:
+    Configuration.dispatcher.update.middleware(UserMiddleware())
