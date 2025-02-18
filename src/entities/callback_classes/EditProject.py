f750a8e67ce4833fff7a701a894cefe0f06f6d5e
VictorVangeli <whgaleon1@yandex.ru>
2025-02-18 10:29:29 +0300

    feat: add new options in EditProject Callback class

--- src/entities/callback_classes/EditProject.py
+++ src/entities/callback_classes/EditProject.py
@@ -1,6 +1,7 @@
 from aiogram.filters.callback_data import CallbackData
 
 from src.entities.enums.edit_action_type_enum import EditActionTypeEnum
+from src.entities.enums.event_enums import EventTypeEnum
 
 
 class ProjectType(CallbackData, prefix="project"):
@@ -13,3 +14,11 @@ class EditProject(ProjectType, prefix="project"):
 
 class ConfirmAction(EditProject, prefix="project"):
     confirmed_action: str = "true"
+
+
+class EditProjectFAT(EditProject, prefix="project"):
+    fat_event_type: EventTypeEnum
+
+
+class ConfirmActionFAT(EditProjectFAT, prefix="project"):
+    confirmed_event: str = "true"
