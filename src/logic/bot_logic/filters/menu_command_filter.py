ebee5f15745db6f7bd20b0836363d8068c622fde
proDream <sushkoos@gmail.com>
2025-02-27 18:09:25 +0400

    docs: added docstring to MenuCommandFilter class
    - Added a detailed docstring explaining the purpose and functionality of the `MenuCommandFilter` class.

--- src/logic/bot_logic/filters/menu_command_filter.py
+++ src/logic/bot_logic/filters/menu_command_filter.py
@@ -6,6 +6,10 @@ from src.utils.text_utils import localize_text_to_button
 
 
 class MenuCommandFilter(Filter):
+    """
+    Filter to determine if a message text matches any of the localized command variants.
+    """
+
     async def __call__(self, message: Message) -> bool:
         command_variants = [
             localize_text_to_button(text_in_yaml="get_main_menu", lang=lang)
