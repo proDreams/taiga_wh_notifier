b561126abe6617f2a6f52e9766b09b72f6adce76
proDream <sushkoos@gmail.com>
2025-03-01 04:09:40 +0400

    feat: enhanced settings management and utility functions
    - Added imports for logging and Singleton from external modules.
    - Updated `Configuration` class to use `generate_strings_dict` from `yaml_utils`.
    - Created new utility functions `get_settings`, `get_strings`, and `get_logger` in `settings.py`.
    - These functions provide centralized access to settings, strings, and logger instances respectively.

--- src/core/settings.py
+++ src/core/settings.py
@@ -1,10 +1,12 @@
+from logging import Logger
+
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from dynaconf import Dynaconf
 
 from src.core.Base.singleton import Singleton
 from src.utils.logger_utils import LoggerUtils
-from src.utils.yaml_utils import get_strings
+from src.utils.yaml_utils import generate_strings_dict
 
 
 class Configuration(Singleton):
@@ -14,6 +16,38 @@ class Configuration(Singleton):
 
     settings = Dynaconf(envvar_prefix=False, environments=True, settings_files=["config/settings.yaml"])
     logger = LoggerUtils(settings=settings)
-    strings = get_strings(path=settings.YAML_FILE_PATH)
+    strings = generate_strings_dict(path=settings.YAML_FILE_PATH)
     bot = Bot(token=settings.TELEGRAM_BOT_TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
     dispatcher = Dispatcher()
+
+
+def get_settings() -> Dynaconf:
+    """
+    Returns the current settings configuration.
+
+    :return: The settings configuration object.
+    :rtype: Dynaconf
+    """
+    return Configuration.settings
+
+
+def get_strings() -> dict:
+    """
+    Returns the dictionary containing string configurations.
+
+    :returns: A dictionary with configuration strings.
+    :rtype: dict
+    """
+    return Configuration.strings
+
+
+def get_logger(**kwargs) -> Logger:
+    """
+    Returns a logger instance based on the provided configuration.
+
+    :param kwargs: Arbitrary keyword arguments passed to the logging library.
+    :type kwargs: dict
+    :returns: Logger instance configured according to the provided parameters.
+    :rtype: Logger
+    """
+    return Configuration.logger.get_logger(**kwargs)
