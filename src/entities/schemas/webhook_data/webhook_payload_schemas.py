8ceaa16d74b1545a5e74e6ebf208fbba8cfb8ca1
VictorVangeli <whgaleon1@yandex.ru>
2025-03-05 00:45:30 +0300

    feat: update schemas to parse webhook. add epic, wiki, issue

--- src/entities/schemas/webhook_data/webhook_payload_schemas.py
+++ src/entities/schemas/webhook_data/webhook_payload_schemas.py
@@ -5,17 +5,20 @@ from pydantic import BaseModel
 
 from src.entities.schemas.webhook_data.base_webhook_schemas import Change
 from src.entities.schemas.webhook_data.nested_schemas import (
+    Epic,
+    Issue,
     Milestone,
     Task,
     User,
     UserStory,
+    Wiki,
 )
 
 
 class WebhookPayload(BaseModel):
     action: str
-    type: Literal["task", "milestone", "userstory"]
+    type: Literal["task", "milestone", "userstory", "epic", "wikipage", "issue"]
     by: User
     date: datetime
-    data: Task | Milestone | UserStory
+    data: Task | Milestone | UserStory | Epic | Wiki | Issue
     change: Change | None = None
