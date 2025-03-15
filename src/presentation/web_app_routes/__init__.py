f3570c3a5acc1fbf42456f76629fee1880717da0
proDream <sushkoos@gmail.com>
2025-03-15 20:52:24 +0400

    refactor: moved router inclusion order
    - Adjusted the order of included routers in the `web_app_router`.

--- src/presentation/web_app_routes/__init__.py
+++ src/presentation/web_app_routes/__init__.py
@@ -5,5 +5,5 @@ from src.presentation.web_app_routes.webhook_route import webhook_router
 
 web_app_router = APIRouter()
 
-web_app_router.include_router(webhook_router)
 web_app_router.include_router(update_router)
+web_app_router.include_router(webhook_router)
