13bff0ba3138d1bbba5cd29548971c760c6ee867
proDream <sushkoos@gmail.com>
2025-03-18 08:32:18 +0400

    fix: issue #242

--- src/entities/schemas/webhook_data/base_webhook_schemas.py
+++ src/entities/schemas/webhook_data/base_webhook_schemas.py
@@ -1,6 +1,9 @@
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+
+from src.core.settings import get_settings
 
 
 class BaseID(BaseModel):
@@ -18,6 +21,26 @@ class BaseName(BaseModel):
 class TimeStamped(BaseModel):
     created_date: datetime | None = None
     modified_date: datetime | None = None
+
+    @field_validator("created_date", mode="before")
+    def created_date_to_local_tz(cls, value: str | datetime | None) -> datetime | None:
+        if value is None:
+            return None
+
+        if isinstance(value, str):
+            value = datetime.fromisoformat(value.replace("Z", "+00:00"))
+
+        return value.astimezone(ZoneInfo(get_settings().TIME_ZONE))
+
+    @field_validator("modified_date", mode="before")
+    def modified_date_to_local_tz(cls, value: str | datetime | None) -> datetime | None:
+        if value is None:
+            return None
+
+        if isinstance(value, str):
+            value = datetime.fromisoformat(value.replace("Z", "+00:00"))
+
+        return value.astimezone(ZoneInfo(get_settings().TIME_ZONE))
 
 
 class BaseRequirement(BaseModel):
